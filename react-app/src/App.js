9c1a4e8ff7621c19bca72e23e448ccc34fb90285
Henry Ni <henryni914@gmail.com>
2020-03-21 23:55:26 -0400

    create index.js and style.css under EmployeeCard folder and created a render for App.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
-import friends from "./employees.json";
+import employees from "./employees.json";
+import Title from "./components/Title";
+import EmployeeCard from "./components/EmployeeCard";
 
 class App extends Component {
 
     state = {
-        friends
+        employees
     };
+
+    render() {
+        return (
+            <div>
+                <Title> Employees List</Title>
+                {this.state.employees.map(element => (
+                <EmployeeCard
+                id = {element.id}
+                key = {element.id}
+                name = {element.name}
+                department = {element.department}
+                role = {element.role}
+                email = {element.email}
+                />
+            ))}
+            </div>
+        );
+    }
 }
 
 export default App;
