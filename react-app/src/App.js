d55b61bcb85d11f0a141f699389b2578b6fd1935
Henry Ni <henryni914@gmail.com>
2020-03-21 23:40:53 -0400

    created employee json and starter App.js

---
+++ react-app/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import friends from "./employees.json";
+
+class App extends Component {
+
+    state = {
+        friends
+    };
+}
+
+export default App;
